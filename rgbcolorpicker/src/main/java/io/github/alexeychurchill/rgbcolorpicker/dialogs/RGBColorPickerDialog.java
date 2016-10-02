9876fed11310bdf7f978eed11e839a38924b69b0
Alexey <samolet15@mail.ru>
2016-10-03 01:51:31 +0300

    Initial commit

--- rgbcolorpicker/src/main/java/io/github/alexeychurchill/rgbcolorpicker/dialogs/RGBColorPickerDialog.java
+++ rgbcolorpicker/src/main/java/io/github/alexeychurchill/rgbcolorpicker/dialogs/RGBColorPickerDialog.java
@@ -7,8 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
-import java.util.Locale;
-
 import io.github.alexeychurchill.rgbcolorpicker.R;
 import io.github.alexeychurchill.rgbcolorpicker.views.RGBColorPickerView;
 
